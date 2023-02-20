0c4f96c78a07ea5bbef3598224b6d6c1edc309e8
Kiriki-liszt <107096260+Kiriki-liszt@users.noreply.github.com>
2023-02-20 23:09:30 +0900

    Update param.h
    unused

--- source/param.h
+++ source/param.h
@@ -10,8 +10,3 @@ enum {
 	kParamOutVuPPM,		// Output VuPPM
 	kParamBypass		// ByPass
 };
-
-static constexpr double Unity = 1.0;
-static constexpr double Factor = 0.7; // GainParamConverter
-static constexpr double db_12p = 3.9810717055349722;
-static constexpr double db_12m = 0.251188643150958;
